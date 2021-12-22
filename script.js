1a8dfda9370ae7e723b5abf821f575d85191cde3
HVetal <HV2011@inbox.ru>
2021-12-22 23:01:25 +0300

    lesson 3 in work

--- script.js
+++ script.js
@@ -3,28 +3,28 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 
 function send(onError, onSuccess, url, method = 'GET', data = '', headers = {}, timeout = 60000) {
- 
+
   let xhr;
 
   if (window.XMLHttpRequest) {
     // Chrome, Mozilla, Opera, Safari
     xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { 
+  } else if (window.ActiveXObject) {
     // Internet Explorer
     xhr = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-  for([key, value] of Object.entries(headers)) {
+  for ([key, value] of Object.entries(headers)) {
     xhr.setRequestHeader(key, value)
   }
 
-  xhr.timeout = timeout; 
+  xhr.timeout = timeout;
 
   xhr.ontimeout = onError;
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
-      if(xhr.status < 400) {
+      if (xhr.status < 400) {
         onSuccess(xhr.responseText)
       } else if (xhr.status >= 400) {
         onError(xhr.status)
@@ -107,6 +107,27 @@ class Cart {
     this.list = []
   }
 
+  _onSuccess(response) {
+    console.log(response);
+    const data = JSON.parse(response);
+    console.log(data);
+    data.contents.forEach(product => {
+      this.list.push(
+        new GoodStack({
+          good: product.contents,
+        })
+      );
+    });
+  }
+
+  _onError(err) {
+    console.log(err);
+  }
+
+  fetchGoods() {
+    send(this._onError, this._onSuccess, `${API_URL}getBasket.json`)
+  }
+  // send((err) => console.log(err), (response) => console.log(response), `${API_URL}getBasket.json`);
   add(good) {
     const idx = this.list.findIndex((stack) => stack.getGoodId() == good.id)
 
@@ -116,6 +137,7 @@ class Cart {
       this.list.push(new GoodStack(good))
     }
   }
+
 
   remove(id) {
     const idx = this.list.findIndex((stack) => stack.getGoodId() == id)
@@ -140,7 +162,11 @@ class Showcase {
     const data = JSON.parse(response)
     data.forEach(product => {
       this.list.push(
-        new Good({id: product.id_product, title: product.product_name, price: product.price})
+        new Good({
+          id: product.id_product,
+          title: product.product_name,
+          price: product.price
+        })
       )
     });
   }
@@ -228,95 +254,97 @@ let sum = 0;
 // showcase.addToCart(3);
 // cart.remove(1);
 setTimeout(() => {
-const draw = new drawShowcaseAll(showcase);
-draw.drawShowcase();
-//console.log(showcase, cart)
+  const draw = new drawShowcaseAll(showcase);
+  draw.drawShowcase();
+  //console.log(showcase, cart)
 
 
 
-// Убрали все кнопки удаления товара из корзины
-const delbtns = document.querySelectorAll('.deleteFromCart');
-for (let i = 0; i < delbtns.length; i++) {
-  delbtns[i].classList.add("unvisible");
-}
-
-const buttonEl = document.querySelector('.goods-list');
-buttonEl.addEventListener('click', event => {
-  const btn = event.target;
-  //const delbtns = document.querySelectorAll('.deleteFromCart');
-  if (btn.tagName !== "BUTTON") {
-    return;
-  } else if (btn.classList.contains('deleteFromCart')) {
+  // Убрали все кнопки удаления товара из корзины
+  const delbtns = document.querySelectorAll('.deleteFromCart');
   for (let i = 0; i < delbtns.length; i++) {
-    // если мы нажали именно на эту кнопку удаления товара
-    if ((btn === delbtns[i])) { // && (showcase.list[i].getId())
-      // если это последний такой товар, то убираем кнопку
-      for (let j = 0; j < cart.list.length; j++) {
-        if ((cart.list[j].count === 1) && (cart.list.length === 1)) {
-          delbtns[i].classList.add("unvisible");
-          //break;
-        } else if ((cart.list[j].count === 1) && (delbtns[j] === delbtns[i]) && (cart.list.length !== 1)) {
-          delbtns[i].classList.add("unvisible");
+    delbtns[i].classList.add("unvisible");
+  }
+
+  const buttonEl = document.querySelector('.goods-list');
+  buttonEl.addEventListener('click', event => {
+    const btn = event.target;
+    //const delbtns = document.querySelectorAll('.deleteFromCart');
+    if (btn.tagName !== "BUTTON") {
+      return;
+    } else if (btn.classList.contains('deleteFromCart')) {
+      for (let i = 0; i < delbtns.length; i++) {
+        // если мы нажали именно на эту кнопку удаления товара
+        if ((btn === delbtns[i])) { // && (showcase.list[i].getId())
+          // если это последний такой товар, то убираем кнопку
+          for (let j = 0; j < cart.list.length; j++) {
+            if ((cart.list[j].count === 1) && (cart.list.length === 1)) {
+              delbtns[i].classList.add("unvisible");
+              //break;
+            } else if ((cart.list[j].count === 1) && (delbtns[j] === delbtns[i]) && (cart.list.length !== 1)) {
+              delbtns[i].classList.add("unvisible");
+            }
+          }
+
+          cart.remove(showcase.list[i].getId());
+          //document.querySelector('.deleteFromCart').classList.add("visible");
+          document.querySelector('.basketCount').textContent = --productInCart;
         }
       }
 
-      cart.remove(showcase.list[i].getId());
-      //document.querySelector('.deleteFromCart').classList.add("visible");
-      document.querySelector('.basketCount').textContent = --productInCart;
+    } else if (btn.classList.contains('addToCart')) {
+      //  const btn = event.target;
+      const btns = document.querySelectorAll('.addToCart');
+      for (let i = 0; i < btns.length; i++) {
+        if (btn === btns[i]) {
+          showcase.addToCart(showcase.list[i].getId());
+          delbtns[i].classList.remove("unvisible");
+          //document.querySelector('.deleteFromCart').classList.add("visible");
+        }
+      }
+      document.querySelector('.basketCount').textContent = ++productInCart;
     }
-  }
-  
-} else if (btn.classList.contains('addToCart')) {
-//  const btn = event.target;
-  const btns = document.querySelectorAll('.addToCart');
-  for (let i = 0; i < btns.length; i++) {
-    if (btn === btns[i]) {
-      showcase.addToCart(showcase.list[i].getId());
-      delbtns[i].classList.remove("unvisible");
-      //document.querySelector('.deleteFromCart').classList.add("visible");
+
+  })
+
+  // const buttonEl = document.querySelector('.goods-list');
+  // buttonEl.addEventListener('click', event => {
+  //   const btn = event.target;
+  //   const btns = document.querySelectorAll('.addToCart');
+  //   if (btn.tagName !== "BUTTON") {
+  //     return;
+  //   }
+  //   for (i = 0; i < btns.length; i++) {
+  //     if (event.target === btns[i]) {
+  //       showcase.addToCart(showcase.list[i].getId());
+  //       delbtns[i].classList.add("visible");
+  //document.querySelector('.deleteFromCart').classList.add("visible");
+  //     }
+  //   }
+  //   document.querySelector('.basketCount').textContent = ++productInCart;
+  // })
+
+  //console.log(showcase, cart)
+
+  //выбираем div, куда будем выводить содержимое корзины
+  const basketProductList = document.querySelector('.basketProductList');
+  // при клике по корзине
+  document.querySelector('.cartIcon').addEventListener('click', event => {
+    //если окно корзины видно на экране
+    if (document.querySelector('.popupBasket').style.display === "block") {
+      //то убрать его
+      document.querySelector('.popupBasket').style.display = "none";
+    } else { //иначе, если окно корзины было скрыто
+      // очистить, что выводилось в предыдущий раз
+      basketProductList.innerHTML = ' ';
+      //показать окно корзины
+      document.querySelector('.popupBasket').style.display = "block";
+      const drawCart = new drawCartAll(cart);
+      drawCart.drawCart();
+      //вывести общую стоимость покупок
+      document.querySelector('.basketTotalValue').textContent = sum;
     }
-  }
-  document.querySelector('.basketCount').textContent = ++productInCart;
-}
-  
-})
+  });
 
-// const buttonEl = document.querySelector('.goods-list');
-// buttonEl.addEventListener('click', event => {
-//   const btn = event.target;
-//   const btns = document.querySelectorAll('.addToCart');
-//   if (btn.tagName !== "BUTTON") {
-//     return;
-//   }
-//   for (i = 0; i < btns.length; i++) {
-//     if (event.target === btns[i]) {
-//       showcase.addToCart(showcase.list[i].getId());
-//       delbtns[i].classList.add("visible");
-      //document.querySelector('.deleteFromCart').classList.add("visible");
-//     }
-//   }
-//   document.querySelector('.basketCount').textContent = ++productInCart;
-// })
-
-//console.log(showcase, cart)
-
-//выбираем div, куда будем выводить содержимое корзины
-const basketProductList = document.querySelector('.basketProductList');
-// при клике по корзине
-document.querySelector('.cartIcon').addEventListener('click', event => {
-  //если окно корзины видно на экране
-  if (document.querySelector('.popupBasket').style.display === "block") {
-    //то убрать его
-    document.querySelector('.popupBasket').style.display = "none";
-  } else { //иначе, если окно корзины было скрыто
-    // очистить, что выводилось в предыдущий раз
-    basketProductList.innerHTML = ' ';
-    //показать окно корзины
-    document.querySelector('.popupBasket').style.display = "block";
-    const drawCart = new drawCartAll(cart);
-    drawCart.drawCart();
-    //вывести общую стоимость покупок
-    document.querySelector('.basketTotalValue').textContent = sum;
-  }
-});
+  console.log(showcase, cart);
 }, 1000)
