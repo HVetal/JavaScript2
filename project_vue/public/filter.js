85096d9cfb1980857d5587b6206148fee1101d8b
HVetal <HV2011@inbox.ru>
2022-01-18 21:49:55 +0300

    lesson 6 in work

--- project_vue/public/filter.js
+++ project_vue/public/filter.js
@@ -1,15 +1,15 @@
-Vue.component(`filter`, {
+Vue.component('filtered', {
     template: `
     <div>
-        <input type="text" id="search-input" v-model="searchLine">
+        <input type="text"  v-model="searchLine">
         <button type="button" v-on:click="setFilter">Искать</button>
     </div>
     `,
-    props: ['list'],
-    // computed: {
-    //     setFilter() {
-    //         return list.RegExp(searchLine, i).test;
-    //     }
+    props: ['list', 'searchLine'],
+    computed: {
+        setFilter() {
+            filteredGoods = list.RegExp(searchLine, 'i').test;
+        }
 
-    // }
+    }
 })
